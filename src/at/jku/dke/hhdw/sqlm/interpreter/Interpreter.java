911e54044f792a445e5f63ac6c1f599f917658b1
Christoph Schütz <schuetz@dke.uni-linz.ac.at>
2015-10-28 14:48:32 +0100

    main function in SQL(M) interpreter not needed.

--- src/at/jku/dke/hhdw/sqlm/interpreter/Interpreter.java
+++ src/at/jku/dke/hhdw/sqlm/interpreter/Interpreter.java
@@ -7,53 +7,6 @@ import at.jku.dke.sqlm.parser.*;
 public class Interpreter {
 	
 	static boolean first=true;
-
-	public static void main(String[] args) {
-		
-		System.out.println("Reading from standard input...");
-	    System.out.print("Enter an M-SQL block:");
-	    
-	    new SQLMParser(System.in);
-	    try {
-	    	long startTime;
-	    	long stopTime;
-	    	long elapsedTime;
-	    	ASTSQLMDocument document = SQLMParser.Start();
-	    	document.dump("");
-			System.out.println("Thank you. \n");
-			
-			startTime = System.currentTimeMillis();
-			Optimizer opt = new Optimizer();
-			opt.run(document);
-			stopTime = System.currentTimeMillis();
-		    elapsedTime = stopTime - startTime;
-		    System.out.println("TIME FOR OPTIMIZATION: "+elapsedTime+"ms\n");
-			document.dump("");
-			
-			System.out.println("----- PL/SQL -----");
-			
-			Translator trans = new Translator();
-			startTime = System.currentTimeMillis();
-			trans.run(document);
-			stopTime = System.currentTimeMillis();
-		    elapsedTime = stopTime - startTime;
-		    System.out.println("TIME FOR TRANSLATION AND EXECUTION: "+elapsedTime+"ms\n");
-			
-			DataAccess dataAccess = DataAccess.getDataAccessMgr();
-			dataAccess.closeConn();
-			System.out.println("----- DONE -----");
-	    }catch (Exception e){
-			try {
-				DataAccess dataAccess = DataAccess.getDataAccessMgr();
-				dataAccess.closeConn();
-			} catch (SQLException e1) {
-				e1.printStackTrace();
-			}
-		    System.out.println("Oops.");
-		    System.out.println(e.getMessage());
-	    } 
-	}
-	
 	
 	public static ResultSet sqlmQuery(oracle.sql.CLOB clob_input) throws Exception{
 		String input = clob_input.getSubString(1, (int)clob_input.length());
